33654cf7fa686acd90cb6a7b3c7f6d77bdf9c17d
Pawel Szczepkowski <paul.szczepkowski@gmail.com>
2021-10-11 22:28:23 +0200

    Fix build

--- components/avatar.js
+++ components/avatar.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
-      <Image src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <Image
+        src={picture}
+        width={100}
+        height={100}
+        className="w-12 h-12 rounded-full mr-4"
+        alt={name}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
