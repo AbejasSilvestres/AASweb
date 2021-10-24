caa75df05850a8a34354cb41a9cd1bcf44226842
thegrinder <paul.szczepkowski@gmail.com>
2021-10-24 19:27:37 +0200

    Make the avatar fill out the entire space

--- components/About/Member.js
+++ components/About/Member.js
@@ -3,8 +3,12 @@ import { getBasePath } from '../../lib/utils';
 export const AboutMember = ({ photo, name, link, description }) => (
   <div>
     <div className="flex gap-8">
-      <div className="w-40 h-40 flex-shrink-0 rounded-full overflow-hidden border-neutral-100 border-2 bg-neutral-100">
-        <img src={`${getBasePath()}${photo}`} alt={name} />
+      <div className="w-40 h-40 flex-shrink-0 rounded-full overflow-hidden border-neutral-300 border-2">
+        <img
+          className="h-full w-full object-cover"
+          src={`${getBasePath()}${photo}`}
+          alt={name}
+        />
       </div>
       <div className="flex-grow">
         <h3 className="font-raleway font-semibold text-xl text-neutral-800 leading-relaxed mb-4">
