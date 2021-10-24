d8e62a92237b8058beb80236edb9c1ff21df8a3c
thegrinder <paul.szczepkowski@gmail.com>
2021-10-24 19:18:05 +0200

    Improve the responsiveness of TwoColumnSection

--- components/TwoColumnSection/TwoColumnSection.js
+++ components/TwoColumnSection/TwoColumnSection.js
@@ -2,7 +2,7 @@ import { SectionContainer } from '../Section';
 
 export const TwoColumnSection = ({ left, right }) => (
   <SectionContainer>
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-40">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-16 md:gap-40">
       <div className="col-span-1 flex justify-center md:justify-end items-center">
         {left}
       </div>
