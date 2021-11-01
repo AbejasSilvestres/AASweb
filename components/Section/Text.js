90ecba222c0c743478c04933dd6ba64f93b1667c
thegrinder <paul.szczepkowski@gmail.com>
2021-11-01 18:23:59 +0100

    Tweak the size of elements in the member screen

--- components/Section/Text.js
+++ components/Section/Text.js
@@ -5,7 +5,7 @@ export const SectionText = ({ as = 'p', children, className, ...rest }) =>
   createElement(
     as,
     {
-      className: classNames('text-lg', 'leading-relaxed', 'block', className),
+      className: classNames('text-xl', 'leading-relaxed', 'block', className),
       ...rest,
     },
     children
--- containers/About/Member.js
+++ containers/About/Member.js
@@ -4,7 +4,7 @@ import { getBasePath } from '../../lib/utils';
 
 export const Member = ({ photo, name, url, description }) => (
   <div>
-    <div className="flex gap-8">
+    <div className="flex flex-col md:flex-row gap-8 md:gap-16">
       <div className="w-40 h-40 flex-shrink-0 rounded-full overflow-hidden border-neutral-300 border-2">
         <img
           className="h-full w-full object-cover"
@@ -13,17 +13,17 @@ export const Member = ({ photo, name, url, description }) => (
         />
       </div>
       <div className="flex-grow items-center">
-        <h3 className="font-semibold text-xl leading-relaxed mb-4 flex items-center">
+        <h3 className="font-semibold text-2xl mb-4">
           <span>{name}</span>
           <ExternalLink
             href={url}
             target="_blank"
-            className="inline-block pl-1.5"
+            className="inline-block pl-1.5 pb-1.5 align-middle"
           >
             <ExternalLinkOutline />
           </ExternalLink>
         </h3>
-        <p className="font-normal text-base leading-relaxed">{description}</p>
+        <p className="font-normal text-lg leading-relaxed">{description}</p>
       </div>
     </div>
   </div>
