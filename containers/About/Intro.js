429ab5a7ddba70a4bbb1c11b9d98ea4e4cf11f00
Pawel Szczepkowski <paul.szczepkowski@gmail.com>
2021-11-13 11:40:06 +0100

    Don't use TwoColumnSection in About.Intro

--- containers/About/Intro.js
+++ containers/About/Intro.js
@@ -3,18 +3,19 @@ import {
   SectionText,
   TwoColumnSection,
   Logo,
+  SectionContainer,
 } from '../../components';
 
 export const Intro = ({ title, body }) => (
-  <TwoColumnSection
-    left={
-      <Logo alt="Logo" size="medium" className="w-72 block text-primary-700" />
-    }
-    right={
-      <div>
+  <SectionContainer>
+    <div className="flex flex-col md:flex-row items-center gap-10">
+      <div className="flex-shrink-0 md:order-last">
+        <Logo alt="Logo" className="w-72 block text-primary-700" />
+      </div>
+      <div className="max-w-screen-sm">
         <SectionTitle className="mb-4">{title}</SectionTitle>
         <SectionText>{body}</SectionText>
       </div>
-    }
-  />
+    </div>
+  </SectionContainer>
 );
