18a19d42e1ff8becea81ca5077e7742e596c2b0e
Pawel Szczepkowski <paul.szczepkowski@gmail.com>
2021-11-13 12:25:46 +0100

    Remove TwoColumnSection

--- components/index.js
+++ components/index.js
@@ -2,6 +2,5 @@ export * from './Container/Container';
 export * from './Layout/Layout';
 export * from './Link/Link';
 export * from './Section';
-export * from './TwoColumnSection/TwoColumnSection';
 export * from './Button/Button';
 export * from './Logo/Logo';
