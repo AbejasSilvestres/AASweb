4624f49c0d14431abd834567ac8baf777eb8a518
Pawel Szczepkowski <paul.szczepkowski@gmail.com>
2021-11-21 18:49:01 +0100

    Fix project header photo

--- containers/Project/Header.js
+++ containers/Project/Header.js
@@ -4,7 +4,7 @@ export const Header = ({ title, coverImage }) => (
   <div className="mt-20 mb-8 md:mb-16 h-96 flex justify-center">
     <img
       src={`${getBasePath()}${coverImage}`}
-      className="max-h-full"
+      className="max-h-full object-contain"
       alt={title}
     />
   </div>
