fc29ea41815c2800a67d92a237cc26a269b46272
thegrinder <paul.szczepkowski@gmail.com>
2021-12-19 19:59:53 +0100

    Set up leaflet map

---
+++ src/containers/Bees/Map.js
@@ -0,0 +1,32 @@
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import data from './data.json';
+
+const Map = () => {
+  return (
+    <MapContainer
+      className="markercluster-map"
+      center={[40.416729, -3.703339]}
+      zoom={6}
+      maxZoom={18}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      <MarkerClusterGroup>
+        {data.map((marker) => {
+          const id = marker[''];
+          return (
+            <Marker
+              key={id}
+              position={[marker.decimalLatitude, marker.decimalLongitude]}
+            />
+          );
+        })}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+};
+export default Map;
--- src/pages/bees.js
+++ src/pages/bees.js
@@ -1,15 +1,33 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import { Container, Layout } from '../components';
 
-const Bees = () => (
-  <>
-    <Head>
-      <title>Abjeas silvestres</title>
-    </Head>
-    <Layout>
-      <Container>Abjeas silvestres</Container>
-    </Layout>
-  </>
-);
+const Bees = () => {
+  const Map = dynamic(
+    () => import('../containers/Bees/Map'), // replace '@components/map' with your component's location
+    { ssr: false } // This line is important. It's what prevents server-side render
+  );
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet/dist/leaflet.css"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css"
+        />
+        <title>Abjeas silvestres</title>
+      </Head>
+      <Layout>
+        <Container>
+          <Map />
+        </Container>
+      </Layout>
+    </>
+  );
+};
 
 export default Bees;
