a53d953ee530207f7b1dde22d2a9934b388a8272
Pawel Szczepkowski <paul.szczepkowski@gmail.com>
2021-12-19 20:25:35 +0100

    Add map (#14)
    * Set up leaflet map
    
    * Move csv file to public folder

--- src/pages/bees.js
+++ src/pages/bees.js
@@ -1,15 +1,36 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import { Container, Layout } from '../components';
+import { getJsonData } from '../lib/api/bees';
 
-const Bees = () => (
-  <>
-    <Head>
-      <title>Abjeas silvestres</title>
-    </Head>
-    <Layout>
-      <Container>Abjeas silvestres</Container>
-    </Layout>
-  </>
-);
+export default function Bees({ data }) {
+  const Map = dynamic(() => import('../containers/Bees/Map'), { ssr: false });
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet/dist/leaflet.css"
+        />
 
-export default Bees;
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/react-leaflet-markercluster/dist/styles.min.css"
+        />
+        <title>Abjeas silvestres</title>
+      </Head>
+      <Layout>
+        <Container>
+          <Map data={data} />
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const data = await getJsonData();
+  return {
+    props: { data },
+  };
+}
