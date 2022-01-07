ee5a83ee5de727477db5b05ef4b4e3725b2f8241
Pawel Szczepkowski <paul.szczepkowski@gmail.com>
2022-01-07 23:45:53 +0100

    Fix relatieve links (#18)

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,5 +4,3 @@ export const getBasePath = () => (isProd ? '/AASweb' : '');
 
 export const formatDate = (dateString) =>
   dateString.substring(0, 10).split('-').reverse().join('/');
-
-export const isExternalLink = (url) => url.startsWith('http');
