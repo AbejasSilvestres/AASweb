c54cec8a9321ba5176a69805d8f9e6b3a6027b4c
thegrinder <paul.szczepkowski@gmail.com>
2022-01-07 20:00:03 +0100

    Order timeline

--- data/_timeline/Abril2019.md
+++ data/_timeline/Abril2019.md
@@ -1,4 +1,5 @@
 ---
+order: 1
 label: Abril 2019
 
 ---
--- src/lib/api/timeline.js
+++ src/lib/api/timeline.js
@@ -9,4 +9,6 @@ export const getTimelineItemSlugs = () => fs.readdirSync(timelineDirectory);
 export const getTimelineItemBySlug = getItemBySlug(timelineDirectory);
 
 export const getAllTimelineItems = (fields = []) =>
-  getTimelineItemSlugs().map((slug) => getTimelineItemBySlug(slug, fields));
+  getTimelineItemSlugs()
+    .map((slug) => getTimelineItemBySlug(slug, fields))
+    .sort((item1, item2) => (item1.order > item2.order ? 1 : -1));
