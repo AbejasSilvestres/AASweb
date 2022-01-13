53fea07767723928ce9aaf81f2769384869fc3a0
thegrinder <paul.szczepkowski@gmail.com>
2022-01-13 23:16:41 +0100

    Add proper images

--- src/pages/resources/bees-guide.js
+++ src/pages/resources/bees-guide.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
 import { Layout, Container } from '../../components';
 import { getAllBeesGuideSections } from '../../lib/api/bees-guide';
+import { getJsonData } from '../../lib/api/bees-guide-data';
 import { BeesGuide } from '../../containers';
 import markdownToHtml from '../../lib/markdown-to-html';
+import { getBasePath } from '../../lib/utils';
 
-export default function Guide({ intro }) {
+export default function Guide({ intro, data }) {
   return (
     <>
       <Head>
@@ -14,17 +16,42 @@ export default function Guide({ intro }) {
         <Container className="pt-24 pb-12">
           <BeesGuide.Intro content={intro} />
         </Container>
+        <ul>
+          {data.map(
+            ({ species, image, butt, psithyrus, description, location }) => (
+              <li key={species} className="mb-8">
+                <span className="block text-lg italic font-semibold">
+                  {species}
+                </span>
+                <img
+                  src={`${getBasePath()}/assets/bees-guide/${image}`}
+                  alt={species}
+                />
+                <span className="block">
+                  {JSON.stringify({
+                    butt,
+                    psithyrus,
+                    description,
+                    location,
+                  })}
+                </span>
+              </li>
+            )
+          )}
+        </ul>
       </Layout>
     </>
   );
 }
 
 export async function getStaticProps() {
+  const data = await getJsonData();
   const { content } = getAllBeesGuideSections(['content'])[0];
   const parsedBeesGuideIntro = await markdownToHtml(content || '');
 
   return {
     props: {
+      data,
       intro: parsedBeesGuideIntro,
     },
   };
