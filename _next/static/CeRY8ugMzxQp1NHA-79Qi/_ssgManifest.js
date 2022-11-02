26774f7060acecfad619497338a6d5cd491ee673
thegrinder <thegrinder@users.noreply.github.com>
2022-11-02 11:46:22 +0000

    deploy: d27beeef58de4e20c42fdada51952a819ce43bce

---
+++ _next/static/CeRY8ugMzxQp1NHA-79Qi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fresources","\u002Fabout","\u002Fiberian-bees","\u002F","\u002Fposts","\u002Fprojects","\u002Fresources\u002Fbees-guide","\u002Fposts\u002F[slug]","\u002Fprojects\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
